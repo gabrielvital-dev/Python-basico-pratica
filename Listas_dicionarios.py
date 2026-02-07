a4fe27b9be5d79f92d4f12f7bf499b40fa898d17
Gabriel Duarte Vital <gabriel.vital.duarte@gmail.com>
2026-02-07 12:08:36 -0300

    Listas e dicionários

---
+++ Listas_dicionarios.py
@@ -0,0 +1,12 @@
+alunos = ["Ana", "João", "Carlos"]
+
+for aluno in alunos:
+    print("Aluno:", aluno)
+
+usuario = {
+    "nome": "Gabriel",
+    "idade": 22,
+    "curso": "ADS"
+}
+
+print("Usuário:", usuario)
